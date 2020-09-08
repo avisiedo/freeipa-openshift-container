54cba7d8711ed34726eafecf9942669ce2b54379
Alejandro Visiedo <avisiedo@redhat.com>
2020-09-08 20:25:30 +0200

    Update current state. Install process is running until the end, inside the init container.

--- init-wrapper.sh
+++ init-wrapper.sh
@@ -12,9 +12,10 @@ function run_bash
     unset INIT_WRAPPER
     exec /bin/bash "$@"
 }
-
-[ "${INIT_WRAPPER}" == "1" ] && run_init_wrapper exit-on-finished -U -r EXAMPLE.TEST --setup-dns --no-forwarders --auto-reverse --allow-zone-overlap --no-ntp
+# exit-on-finished -U -r "${IPA_SERVER_HOSTNAME}" --setup-dns --no-forwarders --auto-reverse --allow-zone-overlap --no-ntp
+[ "${INIT_WRAPPER}" == "1" ] && run_init_wrapper exit-on-finished -U -r "${IPA_SERVER_HOSTNAME}" --setup-dns --no-forwarders --auto-reverse --allow-zone-overlap --no-ntp
 [ "${INIT_WRAPPER}" == "2" ] && run_bash "$@"
-
 unset INIT_WRAPPER
+
+ls -l /usr/sbin/original/init /usr/lib/systemd/systemd
 exec /usr/sbin/original/init "$@"
